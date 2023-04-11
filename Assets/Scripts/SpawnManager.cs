e2131c2ecb05005a83d4c07ea2b76438b5844951
Mauricio Andrés Castro Morales <maocastro13@gmail.com>
2023-04-11 12:43:05 -0500

    Commit1 4.4 Cheack

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,12 +7,27 @@ public class SpawnManager : MonoBehaviour
     private float spawnRange = 9;
 
     public GameObject enemyPrefab;
+    public int enemyCount;
+    public int waveNumber = 1;
+    public GameObject powerupPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
 
+    void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0) 
+        { 
+            waveNumber++; 
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
+    }
    private Vector3 GenerateSpawnPosition ()
    {
         float spawnPosX = Random.Range( -spawnRange, spawnRange);
@@ -21,5 +36,13 @@ public class SpawnManager : MonoBehaviour
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
 
         return randomPos;
+   }
+
+   void SpawnEnemyWave (int enemiesToSpawn) 
+   {
+    for (int i = 0; i < enemiesToSpawn; i++)
+    {
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+    }
    }
 }
